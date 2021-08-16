edd25ae478033a7a3cea2002bbc90ad3c20378d6
serg-yalosovetsky <serg.yalosovetsky@gmail.com>
2021-08-16 10:58:04 +0300

    init

--- camus/routes2.py
+++ camus/routes2.py
@@ -44,42 +44,14 @@ async def index():
         'chat.html', create_room_form=create_room_form)
 
 
-@bp.route('/create_room', methods=['POST'])
-async def new_room():
-    input_data = json.loads(request.get_json())
-
-    # if create_room_form.validate_on_submit():
-    # form = create_room_form
-    name = form.room_name.data
-    password = form.password.data
-    is_public = form.public.data
-    guest_limit = form.guest_limit.data
-
-    try:
-        room = Room(guest_limit=guest_limit, is_public=is_public)
-        room.set_name(name)
-        if password:
-            room.set_password(password)
-        db.session.add(room)
-        commit_database(reraise=True)
-
-        url_to_redirect = f'/room/{room.slug}'
-        out_data =  {'name': name, 'password':password, 'is_public': is_public, 'guest_limit': guest_limit}
-        return jsonify(out_data)
-        # return redirect('/room/{}'.format(room.slug), code=307)
-    except sqlalchemy.exc.IntegrityError:
-        await jsonify(f'The room name "{name}" is not available')
-
-
-
 # The `/chat` route is deprecated.
-# @bp.route('/chat')
-# async def chat_index():
-#     return redirect('/', code=307)
+@bp.route('/chat')
+async def chat_index():
+    return redirect('/', code=307)
 
 
 # The `/chat/` route is deprecated. Prefer`/room/` instead.
-# @bp.route('/chat/<room_id>', methods=['GET', 'POST'])
+@bp.route('/chat/<room_id>', methods=['GET', 'POST'])
 @bp.route('/room/<room_id>', methods=['GET', 'POST'])
 async def room(room_id):
     room = Room.query.filter_by(slug=room_id).first_or_404()
@@ -122,70 +94,10 @@ async def room(room_id):
         status_code)
 
 
-@bp.route('/chat/<room_id>', methods=['POST'])
-async def chat(room_id):
-    room = Room.query.filter_by(slug=room_id).first_or_404()
-
-    if room.is_full():
-        return 'Guest limit already reached', 418
-
-    # No password is required to join the room
-    client = room.authenticate()
-    if not client:
-        status_code = 200
-        form = JoinRoomForm()
-        if form.validate_on_submit():
-            password = form.password.data
-            client = room.authenticate(password)
-
-    if client:
-        db.session.add(client)
-        commit_database(reraise=True)
-        session['id'] = client.uuid
-
-        return f'user added to {room.name}', 200
-
-    else:
-        # Authentication failed
-        status_code = 401
-        return 'Invalid password', 401
-
-
-
-# The `/chat/` route is deprecated. Prefer`/room/` instead.
-@bp.websocket('/room/<room_id>/ws')
-async def room_ws(room_id):
-    # Verify that the room exists
-    Room.query.filter_by(slug=room_id).first_or_404()
-
-    # Verify the client using a secure cookie
-    client = Client.query.filter_by(uuid=session.get('id', None)).first()
-    if client:
-        logging.info(f'Accepted websocket connection for client {client.uuid}')
-        await websocket.accept()
-    else:
-        return 'Forbidden', 403
-
-    inbox, outbox = message_handler.inbox, message_handler.outbox
-
-    send_task = asyncio.create_task(
-        copy_current_websocket_context(ws_send)(outbox[client.uuid]),
-    )
-    receive_task = asyncio.create_task(
-        copy_current_websocket_context(ws_receive)(client.uuid, inbox),
-    )
-    try:
-        await asyncio.gather(send_task, receive_task)
-    finally:
-        logging.info(f'Terminating websocket connection for client {client.uuid}')
-        send_task.cancel()
-        receive_task.cancel()
-
-
-
 # The `/chat/` route is deprecated. Prefer`/room/` instead.
 @bp.websocket('/chat/<room_id>/ws')
-async def chat_ws(room_id):
+@bp.websocket('/room/<room_id>/ws')
+async def room_ws(room_id):
     # Verify that the room exists
     Room.query.filter_by(slug=room_id).first_or_404()
 
@@ -220,13 +132,6 @@ async def public():
     return await render_template(
         'public.html', title='Camus Video Chat | Public Rooms',
         public_rooms=public_rooms)
-
-
-@bp.route('/public_api')
-async def public_api():
-    public_rooms = Room.query.filter_by(is_public=True).all()
-
-    return jsonify({'public_rooms':public_rooms})
 
 
 async def ws_send(queue):
